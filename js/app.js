65201fa3afa03efb0771333201f7697fc03237e0
Tamirlan Mukashev <tamirlan_mukashev@mail.ru>
2023-04-12 01:12:03 +0600

    v1.0.0

---
+++ js/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let tabsWrapper = document.querySelector('.tabs');
+    let tabLinks = document.querySelectorAll('.tabs__link');
+    let tabBodies = document.querySelectorAll('.tabs__body');
+
+    let tabsClasses = [
+        'green',
+        'orange',
+        'blue'
+    ];
+
+    let removeClass = (list, className) => {
+        list.forEach(item => {
+            item.classList.remove(className);
+        })
+    }
+
+    let removeClassesTabs = (tabsWrapper, classNames) => {
+        classNames.forEach(className => {
+            tabsWrapper.classList.remove(`tabs--${className}`)
+        });
+    }
+
+    tabLinks.forEach(link => {
+        link.onclick = (e) => {
+            e.preventDefault();
+            let activeClass = 'tabs__link--active';
+            let activeClassBody = 'tabs__body--active';
+            let className = link.getAttribute('data-tabs-class')
+            let tab = document.querySelector(link.getAttribute('href'));
+
+            removeClass(tabLinks, activeClass)
+            removeClass(tabBodies, activeClassBody)
+            removeClassesTabs(tabsWrapper, tabsClasses)
+
+
+            if (className) {
+                tabsWrapper.classList.add(`tabs--${className}`)
+            }
+
+            link.classList.add(activeClass)
+            tab.classList.add(activeClassBody)
+
+
+        }
+    })
+})
---
+++ js/marquee.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.marquee .marquee__text').marquee({
+        duration: 80000,
+        startVisible: true,
+        duplicated: true
+    });
+});
